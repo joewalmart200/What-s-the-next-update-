772ada4195aa2e860ffa4908169c62fcca91dd76
joewalmart200 <joew91216@gmail.com>
2026-03-09 22:02:51 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const toggle = document.getElementById("themeToggle");
+
+toggle.onclick = () => {
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+localStorage.setItem("theme","dark")
+}else{
+localStorage.setItem("theme","light")
+}
+}
+
+if(localStorage.getItem("theme") === "dark"){
+document.body.classList.add("dark")
+}

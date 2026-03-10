0d569137f199074f4eee5a0c46d2f5545b1c7829
joewalmart200 <joew91216@gmail.com>
2026-03-09 22:14:38 -0400

    Remove theme toggle functionality from script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const toggle = document.getElementById("themeToggle");
 
-toggle.onclick = () => {
-document.body.classList.toggle("dark");
-
-if(document.body.classList.contains("dark")){
-localStorage.setItem("theme","dark")
-}else{
-localStorage.setItem("theme","light")
-}
-}
-
-if(localStorage.getItem("theme") === "dark"){
-document.body.classList.add("dark")
-}
